b72cf4213ad1a8154f12697de87c5b2f1494d03d
Harshpreet Kaur <114172812+harshpreet14@users.noreply.github.com>
2024-01-10 00:44:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const x = prompt('Enter the first number')
+const y = prompt('Enter the second number')
+const result = 2* Number(x) + Number(y)
+
+
+alert(`The final result is: ${result}`)
